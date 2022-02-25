f97c65ec2d4f8c1ad525b94320c80fa04ae10609
Michal Boczula <michal.boczula0101@gmail.com>
2022-02-25 22:38:06 +0100

    Add project files.

---
+++ JobsCatalog.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JobsCatalog.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ JobsCatalog.Persistance/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JobsCatalog.Persistance
+{
+    public class Class1
+    {
+    }
+}
